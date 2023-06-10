563f054589dbe4dc4ff417cbd8a1edb28607991e
igor.meier@gmail.com <igor@Igors-MBP.lan>
2023-06-10 17:08:09 +0100

    frontend setup

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,11 @@
+import '../common/template/dependencies'
+import React from 'react'
+
+export default props => {
+
+    return (
+        <div className="wrapper">
+            <h1>App</h1>
+        </div>
+    )
+}
