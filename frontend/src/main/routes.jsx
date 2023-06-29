cd54bc9b4b7913d22561b140b481b241088f1c90
igor.meier@gmail.com <igor@Igors-MBP.lan>
2023-06-29 21:35:17 +0100

    finishing project

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
-import App from './app'
+import AuthOrApp from './AuthOrApp'
 import Dashboard from "../dashboard/Dashboard";
 import BillingCycle from "../billing-cycle/BillingCycle";
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={App} >
+        <Route path='/' component={AuthOrApp} >
             <IndexRoute component={Dashboard} />
             <Route path='/billing-cycle' component={BillingCycle} />
         </Route>
